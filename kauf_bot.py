670f2829b88d522459ba366ad024fc6176a79bbb
David <151471840+Daviddasomax@users.noreply.github.com>
2025-02-02 10:46:18 +0100

    Update kauf_bot.py

--- kauf_bot.py
+++ kauf_bot.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# 🚀 Logging für Debugging
+# 🚀 Logging für Debugginga
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # 📌 Chromium & Chromedriver in /tmp/ speichern (da /opt/ gesperrt ist)
